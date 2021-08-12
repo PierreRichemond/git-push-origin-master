93bfe315f834181c2dde1f657b99b0d75494b2d2
Pierre Richemond <pierreambroise@hotmail.fr>
2021-08-12 22:15:10 +0900

    in preogress

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+    #  creates a new List (FAILED - 4)
+    #   assigns a newly created list as @list (FAILED - 5)
+    #   redirects to the created list (FAILED - 6)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
